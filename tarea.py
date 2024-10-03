17701da1cd1b13a085f3d09afc5628afd44250fe
JorgePas2 <jorgealbertopascualfernandez@gmail.com>
2024-10-03 16:13:35 -0600

    Create tarea1.py

---
+++ tarea.py
@@ -0,0 +1,11 @@
+#Realiza un progroma que pida 3 calificaciones y calcule el promedio
+
+x=int(input ("digite un numero: "))
+y=int(input ("digite un numero: "))
+z=int(input ("digite un numero: "))
+
+prom= (x+y+z) / 3
+
+print (prom)
+
+print ("fin del programa")
